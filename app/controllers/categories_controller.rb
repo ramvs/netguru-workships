ab2afcd40688c9e54aef79d20cd2ccba2cca161e
Krystian <krystian.kawa1@gmail.com>
2015-03-03 17:07:56 +0100

    Solved Task 2

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,6 @@
 class CategoriesController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit, :update, :destroy, :create]
-  
+
   expose(:categories)
   expose(:category)
   expose(:product) { Product.new }
@@ -12,26 +12,34 @@ class CategoriesController < ApplicationController
   end
 
   def new
+    redirect_to new_user_session_path unless current_user.admin?
   end
 
   def edit
+    redirect_to new_user_session_path unless current_user.admin?
   end
 
   def create
-    self.category = Category.new(category_params)
+    if current_user.admin?
+      self.category = Category.new(category_params)
 
-    if category.save
-      redirect_to category, notice: 'Category was successfully created.'
-    else
-      render action: 'new'
+      if category.save
+        redirect_to category, notice: 'Category was successfully created.'
+      else
+        render action: 'new'
+      end
+    else redirect_to new_user_session_path
     end
   end
 
   def update
-    if category.update(category_params)
-      redirect_to category, notice: 'Category was successfully updated.'
-    else
-      render action: 'edit'
+    if current_user.admin?
+      if category.update(category_params)
+        redirect_to category, notice: 'Category was successfully updated.'
+      else
+        render action: 'edit'
+      end
+    else redirect_to new_user_session_path
     end
   end
 
